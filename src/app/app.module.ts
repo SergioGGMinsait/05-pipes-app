fcfe756059976b05eb2c859d5792e4ec29bed7dd
Sergio Gonzalez <sgonzalezgo@minsait.com>
2023-12-10 19:12:13 +0100

    feat: pipes de angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,17 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import localeEsEA from '@angular/common/locales/es-EA';
+import localeFrCA from '@angular/common/locales/fr-CA';
+
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData( localeEsEA );
+registerLocaleData( localeFrCA );
 
 @NgModule({
   declarations: [
@@ -10,9 +19,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID, useValue: 'es-EA' // para establecer el idioma por defecto
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
